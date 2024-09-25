a199f96478932ab1fca62904b0e30401c1342851
hafeezahmadyt <hafeezquantum@gmail.com>
2024-09-25 17:13:54 +0500

    #Fstrings

--- practice day 2.py
+++ practice day 2.py
@@ -39,3 +39,17 @@ print(name.lower())
 print(name.strip()) #Removes spaces Str and End
 print(name.replace('hafeez','m'))
 print(name.split("a")) #splits the string where "a" is located
+
+#string merging
+txt1 = "hello"
+txt2 = "how are you"
+full_txt = txt1+" "+txt2
+print(full_txt)
+
+#string format and F string
+amount = 20000
+#txt = "my salary is " + amount will not work
+
+txt = f"my salary is {amount:.3f}" #3f means 3 decimal points
+print(txt)
+txt =f"the sum is {20+20}"
