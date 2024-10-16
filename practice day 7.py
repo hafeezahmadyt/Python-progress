0d813f9b976e979f5f66721914f9a54aea65dc56
hafeezahmadyt <hafeezquantum@gmail.com>
2024-10-16 13:38:52 +0500

    Tuple

---
+++ practice day 7.py
@@ -0,0 +1,22 @@
+#tuple
+Tuple = ('apple','kiwi','banana','mango')
+print(Tuple)
+#tuples are unchangable means we cannot add or remove anything after they are created
+print(len(Tuple))
+
+#create tuple with one item we use , in end
+myTuple =('hafeez',)
+print(myTuple)
+
+#Below is not tuple
+myTuple =('hafeez')
+print(myTuple)
+
+#Tuple can hold multiple data types
+MyTuple = ('hafeez','aziz',12,12,32,False,True,2,4)
+print(MyTuple)
+
+result = MyTuple[2] +  MyTuple[3]
+print(result)
+print(MyTuple[2])
+print(MyTuple[2] +  MyTuple[3])
