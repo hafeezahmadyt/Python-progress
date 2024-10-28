efd89bbc08c1381088cf3504facbdfb03681c2ab
hafeezahmadyt <hafeezquantum@gmail.com>
2024-10-28 15:01:33 +0500

    classes

---
+++ practice day 12.py
@@ -0,0 +1,29 @@
+#In python theres no Arrays we can use list as arrays
+cars = ['Ford','Bolan','BMW']
+x = cars[0]
+print(x)
+
+#Modifying value of first array
+
+cars[0]='Mustang'
+print(cars)
+
+print(len(cars)) #Return Elements of array
+
+#looping through arrays
+for x in cars:
+ print(x)
+
+ #Pop() Method is used for removing items from the list with providing index number
+ cars.pop(1)
+ #Also we can use Remove() Method to remove items by providing item name
+ cars.remove('BMW')
+ print(cars)
+
+#Classes and Objects
+class MyClass:
+ x = 5
+
+#lets create object of it
+p1 = MyClass()
+print(p1.x) 
